c79b73293a1b8eb4d42eb857341781b3bb13cfc8
Kandit <gun.rodwong@gmail.com>
2024-12-07 21:40:51 +0700

    Update database URL to use a placeholder and add logging for configuration settings

--- config/settings.py
+++ config/settings.py
@@ -12,11 +12,13 @@ def load_configurations():
         'technical_weight': float(os.getenv('TECHNICAL_WEIGHT', 0.5)),
         'buy_threshold': float(os.getenv('BUY_THRESHOLD', 20)),
         'sell_threshold': float(os.getenv('SELL_THRESHOLD', -20)),
-        'database_url': os.getenv('DATABASE_URL', 'postgresql://postgres:exvKGkIeEydaQZbPFTKmyIPumdyvgzlh@junction.proxy.rlwy.net:39156/railway'),
+        'database_url': os.getenv('DATABASE_URL', 'your_default_database_url'),
         'alpaca_api_key': os.getenv('ALPACA_API_KEY', 'your_default_api_key'),
         'alpaca_api_secret': os.getenv('ALPACA_API_SECRET', 'your_default_api_secret'),
         'open_api_key': os.getenv('OPEN_API_KEY', 'your_default_open_api_key'),
     }
+
+    logging.info(config)
 
     # Optionally, load settings from a JSON file
     config_file_path = os.getenv('CONFIG_FILE_PATH', 'config/settings.json')
